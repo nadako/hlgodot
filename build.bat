4a5c3bbc907829b09d0fb90303d83cfd7c93a5c5
Dan Korostelev <nadako@gmail.com>
2018-01-30 20:50:07 +0300

    even build a dll

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
-cl /Igodot_headers /I../hashlink/src /Fogodot.obj  /c godot.c
+mkdir bin
+cl /Igodot_headers /I../hashlink/src /Fobin\godot.obj  /c godot.c
+cl /LD bin\godot.obj /link /DLL /OUT:bin\godot.dll /IMPLIB:bin\godot.lib
