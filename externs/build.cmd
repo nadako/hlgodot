7973668d0cd8f65c59bef5c8b71e8cff451fd2d5
Dan Korostelev <nadako@gmail.com>
2018-01-31 13:58:13 +0300

    link against libhl

--- externs/build.cmd
+++ externs/build.cmd
@@ -1,5 +1,4 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
 cl /I..\..\hashlink\src /I..\godot_headers /c hlgodot.c
-link /DLL /OUT:hlgodot.hdll hlgodot.obj
-
+link /DLL /OUT:hlgodot.hdll hlgodot.obj ..\..\hashlink\Release\libhl.lib
